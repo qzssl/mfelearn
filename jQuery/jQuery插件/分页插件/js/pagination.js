f4ef63f920cc0cd0919fcf6dc6c6f4d1d69374fd
qzssl <2597253199@qq.com>
2019-07-12 15:49:51 +0800

    修改

--- jQuery/jQuery插件/分页插件/js/pagination.js
+++ jQuery/jQuery插件/分页插件/js/pagination.js
@@ -2,86 +2,124 @@
     var document = window.document;
     //默认参数
     var defaultOpts = {
-        current:3,//当前页
-        totalPage:5,//总页码
-        activeClass:'active',//选中样式
+        current: 1,//当前页
+        count: 10,//总页码,总数据
+        limit: 5,//每页显示条数
+        activeClass: 'active',//选中样式
         // pageBtnClass:'pagebtn',//按钮样式
         // prevClass:'prev',//上一页按钮样式
         // nextClass:'next',//下一页按钮样式
-        prevText:'上一页',//上一页文字样式
-        nextText:'下一页',//下一页文字样式
+        prevText: '上一页',//上一页文字样式
+        nextText: '下一页',//下一页文字样式
         // firstClass:'first-page',//首页样式
         // endClass:'end-page',//尾页样式
-        firstText:'首页',
-        endText:'尾页',
-        pageBtnNum:5,//每页展现的按钮数
-        coping:false,//是否开启首页尾页（默认不开启）
-        backfn:function (page) {},//回调函数
+        firstText: '首页',
+        endText: '尾页',
+        pageBtnNum: 5,//每页展现的按钮数
+        coping: false,//是否开启首页尾页（默认不开启）
+        backfn: function (page) {
+        },//回调函数
     };
 
-    function Page(obj,opts){
+    function Page(obj, opts) {
         this.elem = obj;
         this.opts = opts;
-        var firstpage = createElem('a',{'class':'first-page'}),
-            prevpage = createElem('a',{'class':'prev-page'}),
-            nextpage = createElem('a',{'class':'next-page'}),
-            endpage = createElem('a',{'class':'end-page'}),
-            boxpage = createElem('div',{'class':'box-page'});
+        var boxpage = createElem('div', {'class': 'box-page'});
         //移除内容
         this.elem.empty();
         //添加元素
-        if (this.opts.coping){
-            this.elem.append(firstpage).append(prevpage).append(boxpage).append(nextpage).append(endpage);
-        }else {
-            this.elem.append(prevpage).append(boxpage).append(nextpage);
-        }
-        //设置HTML内容
-        this.setHtml({
-            '.first-page':this.opts.firstText,
-            '.prev-page':this.opts.prevText,
-            '.next-page ':this.opts.nextText,
-            '.end-page ':this.opts.endText,
-        });
+        this.elem.append(boxpage);
         this.render();
         this.event();
     }
 
     //设置HTML内容
-    Page.prototype.setHtml=function(obj) {
-        return $.each(obj,function (elem,value) {
+    Page.prototype.setHtml = function (obj) {
+        return $.each(obj, function (elem, value) {
             $(elem).html(value)
         })
     };
 
     Page.prototype.render = function () {
+        var current = this.opts.current||1, // 总页数
+            count = this.opts.count,//总数据
+            pagebtnnum = this.opts.pageBtnNum,//显示按钮数
+            consbtn=pagebtnnum-2;//连续按钮
+            limit = this.opts.limit||10,
+            pages = Math.ceil(count / limit) || 1;//每页显示数据
 
-        let current =this.opts.current;
-        if (current<=1){
-            current=1;
-            $('.prev-page').addClass('disabled');
-            $('.next-page').removeClass('disabled');
-        }else if (current>=this.opts.totalPage){
-            current=this.opts.totalPage;
-            $('.next-page').addClass('disabled');
-            $('.prev-page').removeClass('disabled');
-        }else {
-            $('.next-page').removeClass('disabled');
-            $('.prev-page').removeClass('disabled');
+        if (current>pages){//当前页大于总页码，设置当前页等于总页码
+            this.opts.current=current=pages;
         }
+        if (consbtn<0){
+            consbtn=1;
+        }else if (consbtn>pages){
+            consbtn=pages;
+        }
+
         //页码
-        var pagehtml = '';
+        var pagehtml = '',r = "disabled";
+        var options=this.opts;
+        var  view = {
+            firstpage:function(){
+                return '<a href="javascript:;" class="first-page' + " " +(1 == current ? " " + r : "")+'" data-page=1>' + options.firstText + "</a>";
+            }(),
+            prev: function () {
+                return options.prevText ? '<a href="javascript:;" class="prev-page' + (1 == current ? " " + r : "") + '" data-page="' + (current - 1) + '">' + options.prevText + "</a>" : ""
+            }(),
+            page: function () {
+                var e = [];
+                if (count < 1) return "";
+                //左有省略，第一个按钮
+                current==1?
+                    e.push('<span class="page-active">1</span>')
+                :e.push('<a href="javascript:;" data-page="1"  title="&#x9996;&#x9875;">1</a>');
+                var s = pages>pagebtnnum?2:1;
 
-        //第一个
-        if (current==1) {
-            pagehtml+='<a href="javascript:;" class="'+this.opts.activeClass+'" data-pageIndex="1">1</a>'
-        }else{
-            pagehtml+='<a href="javascript:;" data-pageIndex="1">1</a>'
-        }
+                if (consbtn<current&&s>1) {
+                    e.push('<span class="layui-laypage-spr">&#x2026;</span>')
+                }
+                var i = current-Math.ceil(consbtn/2)+1;
+                var k=pages>pagebtnnum?
+                    (current<consbtn+1?2:(pages-current>=consbtn
+                        ?i :pages-consbtn))
+                    :2;
+                var n=pages>pagebtnnum?
+                    (current<consbtn+1?consbtn+1:(pages-current>=consbtn?i+consbtn-1:pages-1))
+                    :pages-1;
+                console.log("开始：",k,n);
+                for (;k<=n;k++){
+                    k === current ? e.push('<span class="page-active">' + k + '</em></span>')
+                        : e.push('<a href="javascript:;" data-page="' + k + '">' + k + "</a>");
 
-        var start,end;
+                }
+                if (s>1&&pages-n>1) {
+                    e.push('<span class="layui-laypage-spr">&#x2026;</span>');
+                }
+                //最后一个按钮
+                return pages>1?pages==current?e.push('<span class="page-active">' + pages + '</em></span>')
+                    : e.push('<a href="javascript:;" title="&#x5C3E;&#x9875;"  data-page="' + pages + '">' + pages+ "</a>"):"",
+                    e.join("")
+            }(),
+            next: function () {
+                return options.nextText ? '<a href="javascript:;" class="next-page' + (current == pages ? " " + r : "") + '" data-page="' + (current + 1) + '">' + options.nextText + "</a>" : ""
+            }(),
+            endpage:function () {
+                return '<a href="javascript:;" class="first-page' + " " +(current == pages ? " " + r : "")+ '" data-page=' + pages + '>' + options.endText + '</a>';
+            }(),
+        };
+        var nav =options.coping? ["firstpage","prev", "page", "next","endpage"]: ["prev", "page", "next"];
+        pagehtml+=[
+            function () {
+                var e = [];
+                return $.each(nav, function (a, t) {
+                    view[t] && e.push(view[t])
+                }),
+                    e.join("")
+            }()].join("")
 
-        //中间部分
-        if (this.opts.totalPage<=this.opts.pageBtnNum){//无省略号
+
+        /*if (this.opts.totalPage<=this.opts.pageBtnNum){//无省略号
             start=2;end=this.opts.totalPage-1;
             pagehtml+=this.setPage(current,start,end)
         }else if (this.opts.pageBtnNum<this.opts.totalPage&&this.opts.totalPage<=this.opts.pageBtnNum+1){//只有一个省略号
@@ -113,51 +151,39 @@
                 end=this.opts.totalPage-1;
                 pagehtml+=this.setPage(current,start,end);
             }
-        }
+        }*/
 
-        //最后一个
-        if (current==this.opts.totalPage){
-            pagehtml+='<a href="javascript:;" class="'+this.opts.activeClass+'" data-pageIndex="'+this.opts.totalPage+'">'+this.opts.totalPage+'</a>'
-        }else{
-            pagehtml+='<a href="javascript:;" data-pageIndex="'+this.opts.totalPage+'">'+this.opts.totalPage+'</a>'
+        this.setHtml({'.box-page': pagehtml});
+        //回调函数
+        if (isFunction(this.opts.backfn)) {
+            let a={
+                current: this.opts.current,
+                limit: this.opts.limit
+            };
+            this.opts.backfn(a)
         }
-        this.setHtml({'.box-page':pagehtml})
     };
+
 
     Page.prototype.setPage=function(current,start,end){
         var str = '';
-        for (;start<=end;start++){
-            if (current==start){
-                str+='<a href="javascript:;" class="'+this.opts.activeClass+'" data-pageIndex="'+start+'">'+start+'</a>'
+        for (var i=start;i<=end;i++){
+            if (current==i){
+                str+='<a href="javascript:;" class="'+this.opts.activeClass+'" data-pageIndex="'+i+'">'+i+'</a>'
             }else{
-                str+='<a href="javascript:;" data-pageIndex="'+start+'">'+start+'</a>'
+                str+='<a href="javascript:;" data-pageIndex="'+i+'">'+i+'</a>'
             }
         }
         return str;
-    }
+    };
 
     Page.prototype.event = function () {
         let self =this;
         this.elem.on('click',function (e) {
             let elem = e.target;
-            if ($(elem).hasClass('prev-page')) { //上一页
-                if (self.opts.current<1){return}
-                self.opts.current-=1;
-            }else if ($(elem).hasClass('next-page')) {//下一页
-                if (self.opts.current>self.opts.totalPage){return}
-                self.opts.current+=1;
-            }else if ($(elem).hasClass('first-page')){
-                if (self.opts.current<=1){return}
-                self.opts.current=1;
-            } else if ($(elem).hasClass('end-page')) {
-                if (self.opts.current>self.opts.totalPage){return}
-                self.opts.current=self.opts.totalPage;
-            }else if ($(elem).attr('data-pageindex')!=undefined) {
-                self.opts.current=parseInt($(elem).attr('data-pageindex'));
-            }
-            if (isFunction(self.opts.backfn)) {
-                self.opts.backfn(self.opts.current)
-            }
+            var index = 0 | parseInt($(elem).attr("data-page"));
+            if (index<2&&self.opts.current==1||index>=pages&&self.opts.current==pages){return}
+            self.opts.current=index;
             self.render();
         })
     };
@@ -173,7 +199,6 @@
 
     //判断是否是函数
     function isFunction(fn) {
-        console.log()
         return Object.prototype.toString.call(fn).slice(8,-1)==='Function';
     }
 
